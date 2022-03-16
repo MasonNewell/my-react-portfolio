9d7fc6ff6771a656fbb1236bb7e902c173e7f1b6
Mason Newell <mason.newell13@gmail.com>
2022-03-16 10:47:28 -0700

    added items/css

--- portfolio/src/components/SkillsList.js
+++ portfolio/src/components/SkillsList.js
@@ -4,19 +4,19 @@ import { Container, ListGroup } from "react-bootstrap";
 function SkillsList() {
   return (
     <Container className="d-flex justify-content-center text-center">
-      <ListGroup>
+      <ListGroup className="list-group-flush">
+        <h4>Proficiencies</h4>
         <ListGroup.Item>React</ListGroup.Item>
         <ListGroup.Item>Node</ListGroup.Item>
         <ListGroup.Item>Express</ListGroup.Item>
-        <ListGroup.Item>RestAPI</ListGroup.Item>
+        <ListGroup.Item>REST API</ListGroup.Item>
         <ListGroup.Item>jQuery</ListGroup.Item>
         <ListGroup.Item>MySQL with Sequelize</ListGroup.Item>
         <ListGroup.Item>NoSQL with MongoDB/Mongoose</ListGroup.Item>
         <ListGroup.Item>IndexedDB</ListGroup.Item>
         <ListGroup.Item>GraphQL</ListGroup.Item>
         <ListGroup.Item>Handlebars</ListGroup.Item>
-        <ListGroup.Item>Bootsrap</ListGroup.Item>
-        <ListGroup.Item></ListGroup.Item>
+        <ListGroup.Item>Bootstrap</ListGroup.Item>
       </ListGroup>
     </Container>
   );
