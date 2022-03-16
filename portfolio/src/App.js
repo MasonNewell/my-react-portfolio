b13bf1478b3699266b22536d1f7932e2b447c4c9
Mason Newell <mason.newell13@gmail.com>
2022-03-16 12:03:57 -0700

    added navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
     <Router>
+      <NavigationBar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
