fc43ec87ba3fad4679b8abff3d133849ef96ccad
Mason Newell <mason.newell13@gmail.com>
2022-03-16 10:16:50 -0700

    added bootstrap

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Footer() {
   return (
-    <div className="fixed-bottom bg-white px-4">
-      <a href="https://github.com/MasonNewell" target="_blank">
-        <FaGithub
-          size="2.5rem"
-          color="black"
-          onMouseOver={({ target }) => (target.style.color = "gray")}
-          onMouseOut={({ target }) => (target.style.color = "black")}
-        />
-      </a>
-      <a href="https://www.linkedin.com/in/mason-newell-a730ab21b/" target="_blank">
-        <FaLinkedin
-          size="2.5rem"
-          onMouseOver={({ target }) => (target.style.color = "rgb(40, 140, 178)")}
-          onMouseOut={({ target }) => (target.style.color = "rgb(40, 103, 178)")}
-        />
-      </a>
-    </div>
+    <Container className="d-flex justify-content-center">
+      <div className="fixed-bottom bg-white px-4">
+        <a href="https://github.com/MasonNewell" target="_blank">
+          <FaGithub
+            size="2.5rem"
+            color="black"
+            onMouseOver={({ target }) => (target.style.color = "gray")}
+            onMouseOut={({ target }) => (target.style.color = "black")}
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/mason-newell-a730ab21b/" target="_blank">
+          <FaLinkedin
+            size="2.5rem"
+            onMouseOver={({ target }) => (target.style.color = "rgb(40, 140, 178)")}
+            onMouseOut={({ target }) => (target.style.color = "rgb(40, 103, 178)")}
+          />
+        </a>
+      </div>
+    </Container>
   );
 }
