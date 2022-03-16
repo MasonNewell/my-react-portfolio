93df22e72083efeac9a51812f4c62a4588f576f6
Mason Newell <mason.newell13@gmail.com>
2022-03-16 11:50:12 -0700

    added about-me paragraph

---
+++ portfolio/src/components/AboutMe.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function AboutMe() {
+  return (
+    <Container className="text-center">
+      <h5>About Me</h5>
+      <p>
+        Web Developer who loves solving difficult problems and finding efficient ways to complete
+        tasks. I have experience working with servers, databases, and other backend technologies. I
+        enjoy creating a seamless user experience through a detailed and efficient back-end while
+        creating a aesthetically pleasing front-end presentation to the user. I enjoy learning new
+        frameworks and libraries on my free time as well as challenging myself to do something new.
+      </p>
+    </Container>
+  );
+}
+
+export default AboutMe;
