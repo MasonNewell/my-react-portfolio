48aa8b112647002a083ccac30b74a1f7ae6fb90c
Mason Newell <mason.newell13@gmail.com>
2022-03-16 19:49:43 -0700

    added image with css

---
+++ portfolio/src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import hero from "../images/hero.jpg";
+import "../styles/hero.css";
+
+function Hero() {
+  return (
+    <>
+      <div className="hero-image"></div>
+    </>
+  );
+}
+
+export default Hero;
