79a51c445ef710fde7e155de14b7a0425101e034
Mason Newell <mason.newell13@gmail.com>
2022-03-16 19:36:46 -0700

    added img, added column specs

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
+import Portrait from "../images/portrait.jpg";
 
 function AboutMe() {
   return (
     <Container className="text-center">
-      <h5>About Me</h5>
-      <p>
-        Web Developer who loves solving difficult problems and finding efficient ways to complete
-        tasks. I have experience working with servers, databases, and other backend technologies. I
-        enjoy creating a seamless user experience through a detailed and efficient back-end while
-        creating a aesthetically pleasing front-end presentation to the user. I enjoy learning new
-        frameworks and libraries on my free time as well as challenging myself to do something new.
-      </p>
+      <Row>
+        <Col lg={6} md={12}>
+          <img src={Portrait}></img>
+        </Col>
+
+        <Col lg={6} md={12}>
+          <h5>About Me</h5>
+          <p>
+            Web Developer who loves solving difficult problems and finding efficient ways to
+            complete tasks. I have experience working with servers, databases, and other backend
+            technologies. I enjoy creating a seamless user experience through a detailed and
+            efficient back-end while creating a aesthetically pleasing front-end presentation to the
+            user. I enjoy learning new frameworks and libraries on my free time as well as
+            challenging myself to do something new.
+          </p>
+        </Col>
+      </Row>
     </Container>
   );
 }
