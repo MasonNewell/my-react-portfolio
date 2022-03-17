23e22f25a8f70d213027a34cafa2955f9840797d
Mason Newell <mason.newell13@gmail.com>
2022-03-16 19:49:58 -0700

    added paths

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,13 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import NavigationBar from "./components/NavigationBar";
+import PortfolioList from "./pages/Portfolio/PortfolioList";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <Router>
       <NavigationBar />
+      <Hero />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/portfolio" element={<PortfolioList />}></Route>
       </Routes>
       <Footer />
     </Router>
