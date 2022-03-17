490ddee28ef20c6bc0e1ad75fa29417f969ec25f
Mason Newell <mason.newell13@gmail.com>
2022-03-16 20:13:17 -0700

    added portfolio item

--- portfolio/src/pages/Portfolio/PortfolioList.js
+++ portfolio/src/pages/Portfolio/PortfolioList.js
@@ -1,7 +1,15 @@
 import React from "react";
+import roomf from "../../images/Projects/roomf.png";
+import budgetTracker from "../../images/Projects/budgetTracker.png";
+import techBlog from "../../images/Projects/techBlog.png";
+import workoutTracker from "../../images/Projects/workoutTracker.png";
 
-function PortfolioList() {
-  return <div>PortfolioList</div>;
-}
+const PortfolioList = [
+  {
+    img: roomf,
+    title: "RoomFinders",
+    text: "Uses Roomster API to find rental properties",
+  },
+];
 
 export default PortfolioList;
