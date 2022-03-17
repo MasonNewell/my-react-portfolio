71cc4661aa4303baf89f16729ec59b3900ee5912
Mason Newell <mason.newell13@gmail.com>
2022-03-16 19:37:06 -0700

    added about-me component

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SkillsList from "../components/SkillsList";
+import AboutMe from "../components/AboutMe";
 
 function Home() {
   return (
     <>
       <h1>Home</h1>
+      <AboutMe />
       <SkillsList />
     </>
   );
