c59f656d47b49e980f94afc8f37ed0e31a8854db
Mason Newell <mason.newell13@gmail.com>
2022-03-20 14:06:18 -0700

    added github links

--- portfolio/src/pages/Portfolio/PortfolioList.js
+++ portfolio/src/pages/Portfolio/PortfolioList.js
@@ -13,41 +13,49 @@ const PortfolioList = [
     img: sportsTracker,
     title: "Sports Tracker",
     text: "React/Node/Express/MySQL",
+    githubLink: "https://github.com/MasonNewell/sports-tracker",
   },
   {
     img: roomf,
     title: "Room Finder",
     text: "HTML/CSS/JS",
+    githubLink: "https://github.com/MasonNewell/rental-finder",
   },
   {
     img: budgetTracker,
     title: "Budget Tracker",
     text: "MongoDB/IndexedDB",
+    githubLink: "https://github.com/MasonNewell/online-offline-budget-trackers",
   },
   {
     img: techBlog,
     title: "Tech Blog",
     text: "Node/Express/MySQL",
+    githubLink: "https://github.com/MasonNewell/tech-blog",
   },
   {
     img: workoutTracker,
     title: "Workout Tracker",
     text: "Node/Express/MongoDB",
+    githubLink: "https://github.com/MasonNewell/workout-tracker",
   },
   {
     img: bookSearchEngine,
     title: "Book Search Engine",
     text: "React/Graphql",
+    githubLink: "https://github.com/MasonNewell/book-search-engine",
   },
   {
     img: weatherDashboard,
     title: "Weather Dashboard",
     text: "3rd party API",
+    githubLink: "https://github.com/MasonNewell/Weather-Dashboard",
   },
   {
     img: noteTaker,
     title: "Note Taker",
     text: "Node/Express",
+    githubLink: "https://github.com/MasonNewell/Note-Taker",
   },
 ];
 
