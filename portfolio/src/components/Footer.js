85cac84313bc554b83d0fff81896c0a5e221be92
Mason Newell <mason.newell13@gmail.com>
2022-03-20 13:50:38 -0700

    added margin

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -4,7 +4,7 @@ import { Container, Row, Col } from "react-bootstrap";
 
 export default function Footer() {
   return (
-    <Container>
+    <Container className="my-4">
       <div className="fixed-bottom d-flex justify-content-center bg-white px-4">
         <a href="https://github.com/MasonNewell" target="_blank">
           <FaGithub
