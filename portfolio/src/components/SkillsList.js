01b4928232b323ed7d5cf676a4bb7aaf28b5a53b
Mason Newell <mason.newell13@gmail.com>
2022-04-13 08:46:01 -0700

    removed ListGroup

--- portfolio/src/components/SkillsList.js
+++ portfolio/src/components/SkillsList.js
@@ -10,23 +10,6 @@ import { TiCode } from "react-icons/ti";
 
 function SkillsList() {
   return (
-    // <Container className="d-flex justify-content-center text-center skills-container">
-    //   <ListGroup horizontal={""} className="list-group-flush skills-list">
-    //     <h4>Proficiencies</h4>
-    //     <ListGroup.Item>React</ListGroup.Item>
-    //     <ListGroup.Item>Node</ListGroup.Item>
-    //     <ListGroup.Item>Express</ListGroup.Item>
-    //     <ListGroup.Item>REST API</ListGroup.Item>
-    //     <ListGroup.Item>jQuery</ListGroup.Item>
-    //     <ListGroup.Item>MySQL with Sequelize</ListGroup.Item>
-    //     <ListGroup.Item>NoSQL with MongoDB/Mongoose</ListGroup.Item>
-    //     <ListGroup.Item>IndexedDB</ListGroup.Item>
-    //     <ListGroup.Item>GraphQL</ListGroup.Item>
-    //     <ListGroup.Item>Handlebars</ListGroup.Item>
-    //     <ListGroup.Item>Bootstrap</ListGroup.Item>
-    //   </ListGroup>
-    // </Container>
-
     <Container>
       <Row className="justify-content-around text-center">
         <Col xs={12}>
