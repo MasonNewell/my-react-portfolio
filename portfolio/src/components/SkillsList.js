5165d85df1ce31fa68ed68b70e9a8e6ef40e62a7
Mason Newell <mason.newell13@gmail.com>
2022-04-13 08:45:27 -0700

    changed padding, added icons/header

--- portfolio/src/components/SkillsList.js
+++ portfolio/src/components/SkillsList.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Container, ListGroup, Row, Col } from "react-bootstrap";
 import "../styles/skillsList.css";
-import { FaReact, FaNodeJs } from "react-icons/fa";
+import { FaReact, FaNodeJs, FaBootstrap } from "react-icons/fa";
 import { SiExpress, SiZapier, SiJquery, SiGraphql, SiHandlebarsdotjs } from "react-icons/si";
 import { GrMysql } from "react-icons/gr";
 import { BiCylinder } from "react-icons/bi";
 import { FiDatabase } from "react-icons/fi";
+import { TiCode } from "react-icons/ti";
 
 function SkillsList() {
   return (
@@ -28,38 +29,44 @@ function SkillsList() {
 
     <Container>
       <Row className="justify-content-around text-center">
+        <Col xs={12}>
+          <h3>
+            <TiCode />
+            <br /> Skills & Technologies <br />
+          </h3>
+        </Col>
         <Col sm={12} md={5} className="bg-dark text-white p-3 m-2">
           React <FaReact />
         </Col>
         <Col sm={12} md={5} className="bg-dark text-white p-3 m-2">
           Node <FaNodeJs />
         </Col>
-        <Col sm={12} md={5} className="bg-dark text-white p-3 m-2">
+        <Col sm={12} md={5} className="bg-dark text-white p-2 m-2">
           Express <SiExpress />
         </Col>
-        <Col sm={12} md={5} className="bg-dark text-white p-3 m-2">
+        <Col sm={12} md={5} className="bg-dark text-white p-2 m-2">
           REST API <SiZapier />
         </Col>
-        <Col sm={12} md={5} className="bg-dark text-white p-3 m-2">
+        <Col sm={12} md={5} className="bg-dark text-white p-2 m-2">
           jQuery <SiJquery />
         </Col>
-        <Col sm={12} md={5} className="bg-dark text-white p-3 m-2">
+        <Col sm={12} md={5} className="bg-dark text-white p-2 m-2">
           MySQL with Sequelize <GrMysql />
         </Col>
-        <Col sm={12} md={5} className="bg-dark text-white p-3 m-2">
+        <Col sm={12} md={5} className="bg-dark text-white p-2 m-2">
           NoSQL with MongoDB/Mongoose <BiCylinder />
         </Col>
-        <Col sm={12} md={5} className="bg-dark text-white p-3 m-2">
+        <Col sm={12} md={5} className="bg-dark text-white p-2 m-2">
           IndexedDB <FiDatabase />
         </Col>
-        <Col sm={12} md={5} className="bg-dark text-white p-3 m-2">
+        <Col sm={12} md={5} className="bg-dark text-white p-2 m-2">
           GraphQL <SiGraphql />
         </Col>
-        <Col sm={12} md={5} className="bg-dark text-white p-3 m-2">
+        <Col sm={12} md={5} className="bg-dark text-white p-3 m-1">
           Handlebars <SiHandlebarsdotjs />
         </Col>
-        <Col sm={12} md={5} className="bg-dark text-white p-3 m-2">
-          Bootstrap
+        <Col sm={12} md={5} className="bg-dark text-white p-2 m-2">
+          Bootstrap <FaBootstrap />
         </Col>
       </Row>
     </Container>
