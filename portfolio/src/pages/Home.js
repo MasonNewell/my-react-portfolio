fc8407ec8c939345dcc75796552c0cb9c86baa27
Mason Newell <mason.newell13@gmail.com>
2022-04-26 09:12:19 -0700

    added Offcanvas

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -1,12 +1,28 @@
-import React from "react";
+import { useState, useEffect, React } from "react";
 import SkillsList from "../components/SkillsList";
 import AboutMe from "../components/AboutMe";
+import ContactForm from "../components/ContactForm";
+import { Offcanvas, Button } from "react-bootstrap";
 
 function Home() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       <AboutMe />
       <SkillsList />
+      <Button variant="primary" onClick={handleShow}>
+        Launch
+      </Button>
+      <Offcanvas show={show} onHide={handleClose} className="bg-secondary">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Contact</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <ContactForm />
+        </Offcanvas.Body>
+      </Offcanvas>
     </>
   );
 }
