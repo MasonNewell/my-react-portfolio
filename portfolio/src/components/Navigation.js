e34b7300b7963cbbb3ebd22fb78b05fe9bbc2949
Mason Newell <mason.newell13@gmail.com>
2022-05-04 12:01:20 -0700

    added collapsible menu

--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
-import { ListGroup, Container } from "react-bootstrap";
+import { ListGroup, Container, Navbar, Nav } from "react-bootstrap";
 import "../styles/navbar.css";
 import resume from "../utils/MasonNewell.pdf";
 
 function Navigation() {
   return (
     <>
-      <nav className="bg-dark p-2">
+      <Navbar collapseOnSelect className="custom-nav" expand="md" bg="dark" variant="dark">
         <Container className="d-flex justify-content-between custom-nav">
           <ListGroup horizontal>
             <ListGroup.Item className="bg-dark border-0">
@@ -16,30 +16,33 @@ function Navigation() {
               </Link>
             </ListGroup.Item>
           </ListGroup>
-          <ListGroup horizontal>
-            <ListGroup.Item className="bg-dark border-0">
-              <Link to="/contact" className="text-decoration-none text-white">
-                Contact
-              </Link>
-            </ListGroup.Item>
-            <ListGroup.Item className="bg-dark border-0">
-              <Link to="/portfolio" className="text-decoration-none text-white">
-                Portfolio
-              </Link>
-            </ListGroup.Item>
-            <ListGroup.Item className="bg-dark border-0">
-              <a
-                href={resume}
-                download="MasonNewell.pdf"
-                target="_blank"
-                className="text-decoration-none text-white"
-              >
-                PDF Resume
-              </a>
-            </ListGroup.Item>
-          </ListGroup>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse>
+            <ListGroup horizontal>
+              <ListGroup.Item className="bg-dark border-0">
+                <Link to="/contact" className="text-decoration-none text-white">
+                  Contact
+                </Link>
+              </ListGroup.Item>
+              <ListGroup.Item className="bg-dark border-0">
+                <Link to="/portfolio" className="text-decoration-none text-white">
+                  Portfolio
+                </Link>
+              </ListGroup.Item>
+              <ListGroup.Item className="bg-dark border-0">
+                <a
+                  href={resume}
+                  download="MasonNewell.pdf"
+                  target="_blank"
+                  className="text-decoration-none text-white"
+                >
+                  PDF Resume
+                </a>
+              </ListGroup.Item>
+            </ListGroup>
+          </Navbar.Collapse>
         </Container>
-      </nav>
+      </Navbar>
       <Outlet />
     </>
   );
