5c193c6f66f328451425880aaf6d6699a0365e52
Mason Newell <mason.newell13@gmail.com>
2022-05-17 07:30:44 -0700

    added bs container, row, col

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,8 +1,19 @@
 import ContactForm from "../components/ContactForm";
 import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
 
 function Contact() {
-  return <ContactForm />;
+  return (
+    <>
+      <Container>
+        <Row className="justify-content-center">
+          <Col xs={12} sm={10}>
+            <ContactForm />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default Contact;
