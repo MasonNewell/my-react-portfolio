d0eb2b48f5630abbadf30c1e8185930a63c24b10
Mason Newell <mason.newell13@gmail.com>
2022-05-17 07:25:29 -0700

    removed href for nav

--- portfolio/src/components/NavigationBar.js
+++ portfolio/src/components/NavigationBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import "../styles/navbar.css";
 import resume from "../utils/MasonNewell.pdf";
@@ -7,12 +8,18 @@ function NavigationBar() {
     <>
       <Navbar collapseOnSelect className="custom-nav" expand="md" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">Mason Newell</Navbar.Brand>
+          <Link to="/" className="nav-brand nav-link text-white">
+            Mason Newell
+          </Link>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/contact">Contact</Nav.Link>
-              <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+              <Link to="/contact" className="nav-link">
+                Contact
+              </Link>
+              <Link to="/portfolio" className="nav-link">
+                Portfolio
+              </Link>
               <Nav.Link href={resume} download="MasonNewell.pdf" target="_blank">
                 PDF Resume
               </Nav.Link>
